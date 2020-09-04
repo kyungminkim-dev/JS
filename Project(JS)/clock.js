a2bf914f265c853969fafa641f8e416d7ae8d9a4
Kyungmin Kim <57028248+kyungminkim-dev@users.noreply.github.com>
2020-09-05 02:05:27 +0900

    Update clock.js

--- Project(JS)/clock.js
+++ Project(JS)/clock.js
@@ -6,8 +6,37 @@ function getTime() {
     const minutes = date.getMinutes();
     const hours = date.getHours();
     const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+    const day = getDay(date.getDay());
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds} ${(day)}`;
     
+}
+
+function getDay(n) {
+    answer = ''
+    switch(n) {
+        case 0:
+            answer = '일';
+            break;
+        case 1:
+            answer = '월';
+            break;
+        case 2:
+            answer = '화';
+            break;
+        case 3:
+            answer = '수';
+            break;
+        case 4:
+            answer = '목';
+            break;
+        case 5:
+            answer = '금';
+            break;
+        case 6:
+            answer = '토';
+            break;
+    }
+    return answer
 }
 
 function init() {
